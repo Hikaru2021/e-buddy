c1110fb7a0c6d5752965c57a319063179b227faf
Jose Mari L. Hinolan <josehinvauj@gmail.com>
2023-06-01 22:59:10 +0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -59,7 +59,7 @@
                                 <div>
                                     <button
                                         type="submit"
-                                        class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 border border-transparent rounded-md bg-gradient-to-r from-orange-600 to-orange-100 focus:outline-none hover:opacity-80 focus:opacity-80"
+                                        class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 border border-transparent rounded-md bg-gradient-to-r from-orange-600 to-orange- focus:outline-none hover:opacity-80 focus:opacity-80"
                                         @click="redirectToHome"
                                     >
                                         Log in
