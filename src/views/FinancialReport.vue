dc071d2b54225cf7ed1d017876801a5b20ab5928
dagwenh <114450651+dagwenh@users.noreply.github.com>
2023-06-25 15:02:39 +0800

    Financial Report Page
    Made some adjustments on the UI of the Financial Report page.

--- src/views/FinancialReport.vue
+++ src/views/FinancialReport.vue
@@ -6,8 +6,8 @@
   Please feel free to edit it but please do not stray from the layout if possible.
  -->
 <template>
-  <div>
-    <upper-nav></upper-nav>
+  <upper-nav></upper-nav>
+  <div class="flex flex-col items-center h-screen">
     <div class="flex flex-col items-center">
       <!-- Financial Report Text -->
       <h2 class="text-5xl font-bold mt-4 text-center text-amber-400">FINANCIAL REPORT</h2>
@@ -16,7 +16,7 @@
     <!-- Financial Report Container -->
     <div class="flex justify-center mt-20">
       <!-- Previous Month Button -->
-      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-l" @click="changeMonth(0)">
+      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-l shadow-md" @click="changeMonth(0)">
         &lt;
       </button>
 
@@ -26,63 +26,65 @@
       </div>
 
       <!-- Next Month Button -->
-      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-r" :disabled="isDisabled"
+      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-r shadow-md" :disabled="isDisabled"
         id="button" @click="changeMonth(1)">
         &gt;
       </button>
     </div>
 
-    <!-- Monthly Chart Placeholder -->
-    <div class="flex justify-center items-center mt-10 w-auto text-center">
-      <Pie v-if="render" :data="month" :options="options" />
+   <!-- Monthly Chart Placeholder -->
+    <div class="flex justify-center items-center mt-10 w-1/4">
+      <div class="flex justify-center items-center">
+        <Pie v-if="render" :data="month" :options="options" />
+      </div>
     </div>
 
     <!-- Weekly Charts Container -->
-    <div class="flex justify-center m-10">
-      <div class="border-4 border-amber-500 p-2 rounded">
-        <div class="grid grid-cols-2 gap-3">
+    <div class="flex justify-center m-10" style="width:50%;">
+      <div class="border-2 border-amber-500 p-2 rounded" style="width:75%; height: 100%;" >
+        <div class="grid grid-cols-2 gap-1 justify-items-center">
           <!-- Weekly Chart 1 -->
-          <div>
+          <div class="w-2/4">
             <Pie v-if="render" :data="week1" :options="options" />
             <p class="text-center">Week 1</p>
           </div>
 
           <!-- Weekly Chart 2 -->
-          <div>
+          <div class="w-2/4">
             <Pie v-if="render" :data="week2" :options="options" />
             <p class="text-center">Week 2</p>
           </div>
 
           <!-- Weekly Chart 3 -->
-          <div>
+          <div class="w-2/4">
             <Pie v-if="render" :data="week3" :options="options" />
             <p class="text-center">Week 3</p>
           </div>
 
           <!-- Weekly Chart 4 -->
-          <div>
+          <div class="w-2/4">
             <Pie v-if="render" :data="week4" :options="options" />
             <p class="text-center">Week 4</p>
           </div>
           <!-- Weekly Chart 5 -->
-          <div>
+          <div class="w-2/4">
             <Pie v-if="render" :data="week5" :options="options" />
-            <p class="text-center">Week5</p>
+            <p class="text-center">Week 5</p>
           </div>
         </div>
       </div>
     </div>
 
     <!-- Expenses Table -->
-    <div class="m-10 flex justify-center">
+    <div class="m-5 flex justify-center">
       <div class="w-80% max-w-2xl">
-        <table class="border-collapse">
+        <table class="border-collapse mx-auto">
           <thead>
             <tr>
-              <th class="border border-amber-500 px-4 py-2">Note</th>
-              <th class="border border-amber-500 px-4 py-2">Type</th>
-              <th class="border border-amber-500 px-4 py-2">Date</th>
-              <th class="border border-amber-500 px-4 py-2">Amount</th>
+              <th class="border-2 border-amber-500 px-20 py-2">Note</th>
+              <th class="border-2 border-amber-500 px-20 py-2">Type</th>
+              <th class="border-2 border-amber-500 px-20 py-2">Date</th>
+              <th class="border-2 border-amber-500 px-20 py-2">Amount</th>
             </tr>
           </thead>
           <tbody>
@@ -109,7 +111,7 @@ import { ref } from "vue";
 
 ChartJS.register(ArcElement, Tooltip);
 
-const { expenses, month, week1, week2, week3, week4, week5, error, load } =
+const { expenses, month, week1, week3, week4, week5, error, load } =
   getDb();
 
 const monthNames = [
@@ -142,6 +144,18 @@ const options = {
   maintainAspectRatio: true,
 };
 
+// Define the week2 variable and populate it with data
+const week2 = ref({
+  labels: ["Label 1", "Label 2", "Label 3"],
+  datasets: [
+    {
+      data: [10, 20, 30],
+      backgroundColor: ["#FF0000", "#008000", "#FFA500"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+    },
+  ],
+});
+
 function changeMonth(x) {
   if (x == 0) {
     offset.value--;
@@ -158,200 +172,9 @@ function changeMonth(x) {
 }
 </script>
 
-<script>
-export default {
-  components: {
-    UpperNav
-  }
-  // },
-  // data() {
-  //   return {
-  //     currentMonth: '',
-  //     tableData: []
-  //   }
-  // },
-  // mounted() {
-  //   this.createMonthlyChart()
-  //   this.createWeeklyCharts()
-  //   this.fetchTableData()
-  // },
-  // methods: {
-  //   fetchTableData() {
-  //     console.log('Fetching table data...')
-  //     // Simulate fetching data from an API
-  //     setTimeout(() => {
-  //       // Dummy data for demonstration
-  //       const newData = [
-  //         { id: 1, date: '2023-05-01', data1: 10, data2: 20, data3: 30 },
-  //         { id: 2, date: '2023-05-02', data1: 15, data2: 25, data3: 35 },
-  //         { id: 3, date: '2023-05-03', data1: 12, data2: 22, data3: 32 }
-  //         // Add more data as needed
-  //       ]
-
-  //       this.tableData = newData // Update the table data
-  //     }, 1000) // Simulate a delay of 1 second
-  //   },
-  //   createWeeklyCharts() {
-  //     const chart1Canvas = this.$refs.weeklyChart1
-  //     const chart2Canvas = this.$refs.weeklyChart2
-  //     const chart3Canvas = this.$refs.weeklyChart3
-  //     const chart4Canvas = this.$refs.weeklyChart4
-
-  //     // Dummy data for demonstration
-  //     const labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4']
-  //     const datasets = [
-  //       {
-  //         label: 'Chart 1',
-  //         data: [30],
-  //         backgroundColor: ['red']
-  //       },
-  //       {
-  //         label: 'Chart 2',
-  //         data: [20],
-  //         backgroundColor: ['green']
-  //       },
-  //       {
-  //         label: 'Chart 3',
-  //         data: [10],
-  //         backgroundColor: ['blue']
-  //       },
-  //       {
-  //         label: 'Chart 4',
-  //         data: [40],
-  //         backgroundColor: ['orange']
-  //       }
-  //     ]
-
-  //     new Chart(chart1Canvas, {
-  //       type: 'pie',
-  //       data: {
-  //         labels: labels,
-  //         datasets: [datasets[0]]
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false,
-  //         plugins: {
-  //           legend: {
-  //             display: false // Hide the labels
-  //           }
-  //         }
-  //       }
-  //     })
-
-  //     new Chart(chart2Canvas, {
-  //       type: 'pie',
-  //       data: {
-  //         labels: labels,
-  //         datasets: [datasets[1]]
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false,
-  //         plugins: {
-  //           legend: {
-  //             display: false // Hide the labels
-  //           }
-  //         }
-  //       }
-  //     })
-
-  //     new Chart(chart3Canvas, {
-  //       type: 'pie',
-  //       data: {
-  //         labels: labels,
-  //         datasets: [datasets[2]]
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false,
-  //         plugins: {
-  //           legend: {
-  //             display: false // Hide the labels
-  //           }
-  //         }
-  //       }
-  //     })
-
-  //     new Chart(chart4Canvas, {
-  //       type: 'pie',
-  //       data: {
-  //         labels: labels,
-  //         datasets: [datasets[3]]
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false,
-  //         plugins: {
-  //           legend: {
-  //             display: false // Hide the labels
-  //           }
-  //         }
-  //       }
-  //     })
-  //   },
-  //   createMonthlyChart() {
-  //     const chartCanvas = this.$refs.chartCanvas
-  //     const ctx = chartCanvas.getContext('2d')
-
-  //     // Dummy data for demonstration
-  //     const data = {
-  //       labels: ['Label 1', 'Label 2', 'Label 3'],
-  //       datasets: [
-  //         {
-  //           data: [30, 50, 20],
-  //           backgroundColor: ['red', 'green', 'blue']
-  //         }
-  //       ]
-  //     }
-
-  //     new Chart(ctx, {
-  //       type: 'pie',
-  //       data: data,
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false
-  //       }
-  //     })
-  //   },
-  //   goToNextMonth() {
-  //     const months = [
-  //       'January',
-  //       'February',
-  //       'March',
-  //       'April',
-  //       'May',
-  //       'June',
-  //       'July',
-  //       'August',
-  //       'September',
-  //       'October',
-  //       'November',
-  //       'December'
-  //     ]
-  //     const currentMonthIndex = months.indexOf(this.currentMonth)
-  //     const nextMonthIndex = (currentMonthIndex + 1) % months.length
-  //     this.currentMonth = months[nextMonthIndex]
-  //   },
-  //   goToPreviousMonth() {
-  //     const months = [
-  //       'January',
-  //       'February',
-  //       'March',
-  //       'April',
-  //       'May',
-  //       'June',
-  //       'July',
-  //       'August',
-  //       'September',
-  //       'October',
-  //       'November',
-  //       'December'
-  //     ]
-  //     const currentMonthIndex = months.indexOf(this.currentMonth)
-  //     const previousMonthIndex = (currentMonthIndex - 1 + months.length) % months.length
-  //     this.currentMonth = months[previousMonthIndex]
-  //   }
-  // }
+<style>
+table {
+  table-layout: fixed;
+  width: 100%;
 }
-</script>
+</style>
