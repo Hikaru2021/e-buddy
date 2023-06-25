73d2da0876f3854504319d1e4b381769ff104c00
Jose Mari L. Hinolan <josehinvauj@gmail.com>
2023-06-25 15:56:49 +0800

    change 3
    adad

--- src/views/FinancialReport.vue
+++ src/views/FinancialReport.vue
@@ -10,13 +10,13 @@
   <div class="flex flex-col items-center h-screen">
     <div class="flex flex-col items-center">
       <!-- Financial Report Text -->
-      <h2 class="text-5xl font-bold mt-4 text-center text-amber-400">FINANCIAL REPORT</h2>
+      <h2 class="text-5xl font-bold mt-4 text-center text-black">FINANCIAL REPORT</h2>
     </div>
 
     <!-- Financial Report Container -->
     <div class="flex justify-center mt-20">
       <!-- Previous Month Button -->
-      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-l shadow-md" @click="changeMonth(0)">
+      <button class="bg-black hover:opacity-60 text-white font-bold py-2 px-4 rounded-l shadow-md" @click="changeMonth(0)">
         &lt;
       </button>
 
@@ -26,7 +26,7 @@
       </div>
 
       <!-- Next Month Button -->
-      <button class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-r shadow-md" :disabled="isDisabled"
+      <button class="bg-black hover:opacity-60 text-white font-bold py-2 px-4 rounded-r shadow-md" :disabled="isDisabled"
         id="button" @click="changeMonth(1)">
         &gt;
       </button>
@@ -41,7 +41,7 @@
 
     <!-- Weekly Charts Container -->
     <div class="flex justify-center m-10" style="width:50%;">
-      <div class="border-2 border-amber-500 p-2 rounded" style="width:75%; height: 100%;" >
+      <div class="border-2 border-black p-2 rounded" style="width:75%; height: 100%;" >
         <div class="grid grid-cols-2 gap-1 justify-items-center">
           <!-- Weekly Chart 1 -->
           <div class="w-2/4">
@@ -81,18 +81,18 @@
         <table class="border-collapse mx-auto">
           <thead>
             <tr>
-              <th class="border-2 border-amber-500 px-20 py-2">Note</th>
-              <th class="border-2 border-amber-500 px-20 py-2">Type</th>
-              <th class="border-2 border-amber-500 px-20 py-2">Date</th>
-              <th class="border-2 border-amber-500 px-20 py-2">Amount</th>
+              <th class="border-2 border-black px-20 py-2">Note</th>
+              <th class="border-2 border-black px-20 py-2">Type</th>
+              <th class="border-2 border-black px-20 py-2">Date</th>
+              <th class="border-2 border-black py-2">Amount</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="expense in expenses" :key="expense.id">
-              <td class="border border-amber-500 px-4 py-2">{{ expense.name }}</td>
-              <td class="border border-amber-500 px-4 py-2">{{ expense.type }}</td>
-              <td class="border border-amber-500 px-4 py-2">{{ expense.dateString }}</td>
-              <td class="border border-amber-500 px-4 py-2">{{ expense.amount }}</td>
+              <td class="border border-black px-4 py-2">{{ expense.name }}</td>
+              <td class="border border-black px-4 py-2">{{ expense.type }}</td>
+              <td class="border border-black px-4 py-2">{{ expense.dateString }}</td>
+              <td class="border border-black px-4 py-2">{{ expense.amount }}</td>
             </tr>
           </tbody>
         </table>
